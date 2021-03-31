a9f6199438b8b35166fa4a6c6a4a86803fe48e50
samvidav <samvida.venkatesh@gmail.com>
2021-03-31 16:16:06 +0100

    Add script to build eid x phenotype matrix

--- README.md
+++ README.md
@@ -6,7 +6,8 @@ General scripts in /general_scripts:
 2. **calculate_demographic_covariates.R** - Annotates individual IDs with sex (confirmed by UKB QC file), ancestry, median height, and first 21 genetic PCs from UKB. 
 3. **gp_age_sex_BMI_annotation.R** - Annotates the entire GP clinical UKBIOBANK file with UKBIOBANK-phenotype details on sex, mean BMI from UKBIOBANK measurements, and DOB (with age calculation).
 4. **pregnancy_annotation.R** - Produces list of individuals with pregnancy records in UKB or primary care and estimated dates of pregnancy.
-5. **spiros_phenotype_codes_cleaning.R** - Adds ICD9 codes to the ICD10 codes and priamry care read CTV3 codes to the V2 codes in the cleaned phenotype list produced by Spiros Denaxas group (https://github.com/spiros/chronological-map-phenotypes)
+5. **spiros_phenotype_codes_cleaning.R** - Adds ICD9 codes to the ICD10 codes and priamry care read CTV3 codes to the V2 codes in the cleaned phenotype list produced by Spiros Denaxas group (https://github.com/spiros/chronological-map-phenotypes).
+6. **build_eid_phenotype_matrix.R** - Constructs matrix of 0/1 for presence of disease phenotype code (cleaned, Spiros) in each individual in UKB secondary data and primary care data.
 
 Remaining scripts organised by trait in /scripts:
 
---
+++ general_scripts/build_eid_phenotype_matrix.R
@@ -0,0 +1,110 @@
+# Author: Samvida S. Venkatesh
+# Date: 31/03/21
+
+library(tidyverse)
+
+# Read data ----
+
+# Read dictionary to get to phenotype lists
+dictionary <- read.table("/well/lindgren/UKBIOBANK/samvida/general_resources/UKB_codelists/chronological-map-phenotypes/annot_dictionary.txt",
+                       sep = "\t", header = T, stringsAsFactors = F)
+PHENOTYPES <- dictionary$phenotype
+
+# Read GP clinical file
+primary_care <- read.table("/well/lindgren/UKBIOBANK/DATA/PHENOTYPE/PRIMARY_CARE/gp_clinical.txt",
+                           sep = "\t", header = T, comment.char = "$",
+                           stringsAsFactors = F)
+primary_care$eid <- as.character(primary_care$eid)
+
+# Read UKB phenotype file
+secondary_care <- read.table("/well/lindgren/UKBIOBANK/DATA/PHENOTYPE/PHENOTYPE_MAIN/ukb10844.csv",
+                             header = T, sep = ",", na.string = c("NA", "", "."), 
+                             stringsAsFactors = F)
+colnames(secondary_care) <- gsub("X", "f.", colnames(secondary_care))
+colnames(secondary_care)[1] <- "eid"
+# Subset to ICD-relevant columns
+# f.41202.x.x - main ICD10 diagnosis
+# f.41204.x.x - secondary ICD10 diagnosis
+# f.40001.x.x - underlying (primary) cause of death ICD10
+# f.40002.x.x - contributory (secondary) cause of death ICD10
+# f.41203.x.x - main ICD9 diagnosis
+# f.41205.x.x - secondary ICD9 diagnosis
+ICD9_cols <- c(grep("^f.41203.", colnames(secondary_care)),
+               grep("^f.41205.", colnames(secondary_care)))
+ICD10_cols <- c(grep("^f.41202.", colnames(secondary_care)),
+                grep("^f.41204.", colnames(secondary_care)),
+                grep("^f.40001.", colnames(secondary_care)),
+                grep("^f.40002.", colnames(secondary_care)))
+
+secondary_care <- secondary_care[, c(1, ICD9_cols, ICD10_cols)]
+
+# Construct EID x phenotype matrix ----
+
+ICD9_cols <- c(grep("^f.41203.", colnames(secondary_care)),
+               grep("^f.41205.", colnames(secondary_care)))
+ICD10_cols <- c(grep("^f.41202.", colnames(secondary_care)),
+                grep("^f.41204.", colnames(secondary_care)),
+                grep("^f.40001.", colnames(secondary_care)),
+                grep("^f.40002.", colnames(secondary_care)))
+
+ALL_EIDS <- unique(c(unique(primary_care$eid), secondary_care$eid))
+
+result_matrix <- lapply(1:nrow(dictionary), function (i) {
+  pheno <- dictionary$phenotype[i]
+  
+  # Get phenotype code list for primary care (when the file exists)
+  if (dictionary$CPRD[i] != "") {
+    primary_care_codes <- read.table(paste0("/well/lindgren/UKBIOBANK/samvida/general_resources/UKB_codelists/chronological-map-phenotypes/primary_care/",
+                                            dictionary$annot_CPRD[i]), 
+                                     header = T, sep = "\t", stringsAsFactors = F,
+                                     quote = "")
+    codes_V2 <- unique(primary_care_codes$READV2_CODE)
+    codes_V2 <- codes_V2[codes_V2 != ""]
+    codes_V3 <- unique(primary_care_codes$READV3_CODE)
+    codes_V3 <- codes_V2[codes_V3 != ""]
+    # Get IDs of individuals with these codes recorded in primary care
+    match_ids_primary <- 
+      unique(primary_care$eid[primary_care$read_2 %in% codes_V2 | 
+                                primary_care$read_3 %in% codes_V3])
+  } else match_ids_primary <- NA
+  
+  # Get phenotype code list for secondary care (when the file exists)
+  if (dictionary$ICD[i] != "") {
+    secondary_care_codes <- read.table(paste0("/well/lindgren/UKBIOBANK/samvida/general_resources/UKB_codelists/chronological-map-phenotypes/secondary_care/",
+                                              dictionary$annot_ICD[i]), 
+                                       header = T, sep = "\t", 
+                                       stringsAsFactors = F,
+                                       quote = "")
+    codes_ICD9 <- unique(secondary_care_codes$ICD9)
+    codes_ICD9 <- codes_ICD9[codes_ICD9 != ""]
+    codes_ICD10 <- unique(c(secondary_care_codes$ICD10, 
+                            secondary_care_codes$orig_ICD10code))
+    codes_ICD10 <- codes_ICD10[codes_ICD10 != ""]
+    # Get IDs of individuals with these codes recorded in UKB
+    match_ids_secondary <- apply(secondary_care, 1, function (eid_row) {
+      if (any(codes_ICD9 %in% eid_row[ICD9_cols]) | 
+          any(codes_ICD10 %in% eid_row[ICD10_cols])) {
+        # return the ID
+        res <- eid_row[1]
+      } else res <- NA
+      return (res)
+    })
+    match_ids_secondary <- unique(match_ids_secondary[!is.na(match_ids_secondary)])
+  } else match_ids_secondary <- NA
+  
+  # Get the list of all IDs with phenotype in primary OR secondary care
+  match_ids <- unique(c(match_ids_primary, match_ids_secondary))
+  res <- ALL_EIDS %in% match_ids
+  names(res) <- ALL_EIDS
+  
+  return (res)
+})
+names(result_matrix) <- PHENOTYPES
+result_matrix <- bind_rows(result_matrix)
+result_matrix$phenotype <- PHENOTYPES
+result_matrix <- result_matrix[, c("phenotype", ALL_EIDS)]
+
+# Save results
+write.table(result_matrix, 
+            "/well/lindgren/UKBIOBANK/samvida/general_resources/eid_phenotype_matrix.txt",
+            sep = "\t", quote = F, row.names = F)
