5a43194983e8679cce3dfa4e571d30d725a03618
Samvida S. Venkatesh <samvida.venkatesh@gmail.com>
2022-07-15 10:52:37 +0100

    All strata conditional analysis

--- adiposity/scripts/post_GWAS/gcta_cojo_conditional_analysis.sh
+++ adiposity/scripts/post_GWAS/gcta_cojo_conditional_analysis.sh
@@ -17,7 +17,8 @@ echo "passing covariates..."
 covars=${covars//|/,}
 echo ${covars}
 
-cd /well/lindgren-ukbb/projects/ukbb-11867/samvida/adiposity/2204_models/GWAS/post_GWAS/BMI_sex_comb/classify_lmm_intercept_variants
+cd /well/lindgren-ukbb/projects/ukbb-11867/samvida/adiposity/2204_models/GWAS/post_GWAS/${STRATA}/classify_lmm_intercept_variants
+
 # Load PLINK module
 module load PLINK/2.00a2.3_x86_64
 
@@ -31,10 +32,12 @@ while read var; do
 	# Remove SNP of interest from reported SNP list
 	grep -vwE ${var} \
 	reported_variants/${var}_published.txt > tmp_pub_around_${var}.txt	
-	
+
 	# Create temporary bed/bim/fam files for the relevant genomic region
+	# Only retaining individuals of WB-ancestry for LD calculation
 	plink2 \
 	--bfile /well/lindgren-ukbb/projects/ukbb-11867/samvida/general_resources/UKB_IMPUTED_BED/ukb_imp_chr${CHR}_v3 \
+	--keep /well/lindgren-ukbb/projects/ukbb-11867/samvida/general_resources/fid_iid_white_british.txt \
 	--snp ${var} \
 	--window 1000 \
 	--rm-dup force-first \
@@ -66,64 +69,58 @@ while read var; do
 	--cojo-slct \
 	--out reported_variants/${var}_indpt_reported_snps
 
-	# Extract independent SNP list
-	awk -F '\t' '(NR>1) {print $2}' \
-	reported_variants/${var}_indpt_reported_snps.jma.cojo \
-	> reported_variants/${var}_indpt_reported_snps.txt
-
-	# If our SNP of interest is in the independently associated 
-	# list of SNPs, then it may be novel or refined;
-	# else it is reported
-	if grep -q ${var} reported_variants/${var}_indpt_reported_snps.txt; then 
-
-		# Remove SNP of interest from reported SNP list
-		grep -vwE ${var} \
-		reported_variants/${var}_indpt_reported_snps.txt \
-		 > reported_variants/${var}_indpt_reported_snps_for_cond.txt	
-	
-		# If there are other SNPs in the independently associated list,
-		# our SNP may be refined
-		# else it is potentially novel
-
-		if [ -s reported_variants/${var}_indpt_reported_snps_for_cond.txt ]; then
-		
-			echo ${var} >> potential_refined_snps.txt
-
-			# 1. Effect of SNP of interest, conditioned on remaining SNPs
-	  	/apps/well/gcta/1.91.5beta/gcta_1.91.5beta/gcta64 \
-			--bfile tmp_ukbb_bed/${var}_region  \
-			--cojo-file tmp_sumstats_gcta.txt \
-			--extract reported_variants/${var}_indpt_reported_snps.txt \
-			--cojo-cond reported_variants/${var}_indpt_reported_snps_for_cond.txt \
-			--out condnl_results/${var}_cond_on_pub
-			
-			# 2. Effect of published SNPs, conditioned on variant of interest
-	  	/apps/well/gcta/1.91.5beta/gcta_1.91.5beta/gcta64 \
-			--bfile tmp_ukbb_bed/${var}_region  \
-			--cojo-file tmp_sumstats_gcta.txt \
-			--extract reported_variants/${var}_published.txt \
-			--cojo-cond tmp_${var}.txt \
-			--out condnl_results/pub_cond_on_${var}	
-
-		else 
-
-			echo ${var} >> potential_novel_snps.txt
-
-		fi
-
-		rm reported_variants/${var}_indpt_reported_snps_for_cond.txt	
-
-	else
-	
+	# If there are no independent SNPs then the SNP of interest
+	# is previously reported
+	if [ ! -e reported_variants/${var}_indpt_reported_snps.jma.cojo ]; then
 		echo ${var} >> reported_snp_list.txt
+	else
+		# Extract independent SNP list
+		awk -F '\t' '(NR>1) {print $2}' \
+		reported_variants/${var}_indpt_reported_snps.jma.cojo \
+		> reported_variants/${var}_indpt_reported_snps.txt
 
+		# If our SNP of interest is in the independently associated 
+		# list of SNPs, then it may be novel or refined;
+		# else it is reported
+		if grep -q ${var} reported_variants/${var}_indpt_reported_snps.txt; then 
+			# Remove SNP of interest from reported SNP list
+			grep -vwE ${var} \
+			reported_variants/${var}_indpt_reported_snps.txt \
+			 > reported_variants/${var}_indpt_reported_snps_for_cond.txt	
+		
+			# If there are other SNPs in the independently associated list,
+			# our SNP may be refined
+			# else it is potentially novel
+			if [ -s reported_variants/${var}_indpt_reported_snps_for_cond.txt ]; then
+				echo ${var} >> potential_refined_snps.txt
+
+				# 1. Effect of SNP of interest, conditioned on remaining SNPs
+	  			/apps/well/gcta/1.91.5beta/gcta_1.91.5beta/gcta64 \
+				--bfile tmp_ukbb_bed/${var}_region  \
+				--cojo-file tmp_sumstats_gcta.txt \
+				--extract reported_variants/${var}_indpt_reported_snps.txt \
+				--cojo-cond reported_variants/${var}_indpt_reported_snps_for_cond.txt \
+				--out condnl_results/${var}_cond_on_pub
+				
+				# 2. Effect of published SNPs, conditioned on variant of interest
+	  			/apps/well/gcta/1.91.5beta/gcta_1.91.5beta/gcta64 \
+				--bfile tmp_ukbb_bed/${var}_region  \
+				--cojo-file tmp_sumstats_gcta.txt \
+				--extract reported_variants/${var}_published.txt \
+				--cojo-cond tmp_${var}.txt \
+				--out condnl_results/pub_cond_on_${var}
+
+			else
+				echo ${var} >> potential_novel_snps.txt
+			fi
+		else
+			echo ${var} >> reported_snp_list.txt
+		fi
 	fi
-
 	rm tmp_${var}.txt
 	rm tmp_pub_around_${var}.txt
 	rm tmp_ukbb_bed/${var}_region*
 	rm reported_variants/${var}_indpt_*
-
 done < tmp_variant_list_chr${CHR}.txt 
 
 echo "###########################################################"
--- adiposity/scripts/post_GWAS/submit_classify_reported_novel_variants.sh
+++ adiposity/scripts/post_GWAS/submit_classify_reported_novel_variants.sh
@@ -15,21 +15,31 @@ echo `date`: Executing task ${SGE_TASK_ID} of job ${JOB_ID} on `hostname` as use
 
 ##########################################################################################
 
+module load R-bundle-Bioconductor/3.14-foss-2021b-R-4.1.2
+
+echo "passing covariates..."
+covars=${covars//|/,}
+echo ${covars}
+
 cd /well/lindgren-ukbb/projects/ukbb-11867/samvida/adiposity/2204_models/GWAS
 
-# Get significant (P <= 5E-8) SNPs from BMI sex-comb strata
-zcat BOLT_results/BMI_sex_comb_lmm_intercepts_final.txt.gz | \
+mkdir post_GWAS/${STRATA_NAME}/classify_lmm_intercept_variants
+mkdir post_GWAS/${STRATA_NAME}/classify_lmm_intercept_variants/reported_variants
+
+# Get significant (P <= 5E-8) SNPs 
+zcat BOLT_results/${STRATA_NAME}_lmm_intercepts_final.txt.gz | \
 awk -F '[[:space:]]+' 'NR>1 { if ($9 <= 5E-8) print $1,$2,$3 }' - \
-> post_GWAS/BMI_sex_comb/lmm_intercepts_sig_snps.txt
+> post_GWAS/${STRATA_NAME}/lmm_intercepts_sig_snps.txt
 
 # Create temporary sumstats file in GCTA-COJO format and add column for sample size
-zcat BOLT_results/BMI_sex_comb_lmm_intercepts_final.txt.gz | \
-awk 'BEGIN{FS=OFS="\t"} {print $1, $4, $5, $6, $7, $8, $9, "161926"}' - \
-> post_GWAS/BMI_sex_comb/classify_lmm_intercept_variants/tmp_sumstats_gcta.txt
+zcat BOLT_results/${STRATA_NAME}_lmm_intercepts_final.txt.gz | \
+awk -v n_gp="$N_GP" 'BEGIN{FS=OFS="\t"} {print $1, $4, $5, $6, $7, $8, $9, n_gp}' - \
+> post_GWAS/${STRATA_NAME}/classify_lmm_intercept_variants/tmp_sumstats_gcta.txt
 
 # Create list of reported SNPs within +/- 500kb of every unreported, significant SNP
 # and run GCTA-COJO for conditional analysis by looping over all variants
-Rscript /well/lindgren-ukbb/projects/ukbb-11867/samvida/adiposity/scripts/classify_reported_novel_variants.R
+Rscript /well/lindgren-ukbb/projects/ukbb-11867/samvida/adiposity/scripts/classify_reported_novel_variants.R \
+--strata=${STRATA_NAME}
 
 echo "###########################################################"
 echo "Finished at: "`date`
