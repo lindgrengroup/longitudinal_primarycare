7af7743f7d3045a40d110cbf52de7d2539a682a4
samvidav <samvida.venkatesh@gmail.com>
2024-06-04 15:06:11 +0100

    Fixed line endings

--- adiposity/scripts/manuscript/GWAS/munge_sumstats_for_gwas_catalog.sh
+++ adiposity/scripts/manuscript/GWAS/munge_sumstats_for_gwas_catalog.sh
@@ -13,32 +13,41 @@ echo `date`: Executing task ${SGE_TASK_ID} of job ${JOB_ID} on `hostname` as use
 
 ##########################################################################################
 
+module load Python/3.10.8-GCCcore-12.2.0
+source /well/lindgren/samvida/python/gwas-catalog-sumstats-${MODULE_CPU_TYPE}/bin/activate
+
 cd /well/lindgren-ukbb/projects/ukbb-11867/samvida/adiposity
 
 for ADIPO in BMI Weight; do
 	for SS in F M sex_comb; do
 		# LMM GWASs
 		for LMM in b0 b1; do
-			# old file: 1.SNP 2.CHR 3.BP 4.GENPOS 5.ALLELE1 6.ALLELE0 7.A1FREQ 8.F_MISS 9.CHISQ_LINREG 10.P_LINREG 11.BETA 12.SE 13.CHISQ_BOLT_LMM_INF 14.P_BOLT_LMM_INF
+			# old file: 1.SNP 2.CHR 3.POS 4.Tested_Allele 5.Other_Allele 6.AF_Tested 7.BETA 8.SE 9.PVALUE
 			# new file: chromosome base_pair_location effect_allele other_allele beta standard_error effect_allele_frequency p_value rs_id
-			zcat 2211_models/GWAS/BOLT_results/${ADIPO}_${SS}_${LMM}_filtered.txt.gz \
-			| awk -F'\t' -v OFS='\t' '{print $2, $3, $5, $6, $11, $12, $7, $14, $1}' \
-			> sumstats_gwas_catalog_deposit/${ADIPO}_${SS}_${LMM}_filtered.tsv
+			awk -F'\t' -v OFS='\t' '{print $2, $3, $4, $5, $7, $8, $6, $9, $1}' 2211_models/GWAS/BOLT_results/${ADIPO}_${SS}_${LMM}_final.txt \
+			> sumstats_gwas_catalog_deposit/${ADIPO}_${SS}_${LMM}_sumstats.tsv
 			sed -i '1s/.*/chromosome\tbase_pair_location\teffect_allele\tother_allele\tbeta\tstandard_error\teffect_allele_frequency\tp_value\trs_id/' \
-			sumstats_gwas_catalog_deposit/${ADIPO}_${SS}_${LMM}_filtered.tsv
-			gzip sumstats_gwas_catalog_deposit/${ADIPO}_${SS}_${LMM}_filtered.tsv
+			sumstats_gwas_catalog_deposit/${ADIPO}_${SS}_${LMM}_sumstats.tsv
+
+			# Validate
+			gwas-ssf validate --errors-out sumstats_gwas_catalog_deposit/${ADIPO}_${SS}_${LMM}_sumstats.tsv
+			# gzip
+			gzip sumstats_gwas_catalog_deposit/${ADIPO}_${SS}_${LMM}_sumstats.tsv
 		done
 
 		# Cluster belonging GWASs
 		for CLUST in k1 k1_k2 k1_k2_k3; do
-			# old file: 1.SNP 2.CHR 3.BP 4.GENPOS 5.ALLELE1 6.ALLELE0 7.A1FREQ 8.F_MISS 9.CHISQ_LINREG 10.P_LINREG 11.BETA 12.SE 13.CHISQ_BOLT_LMM_INF 14.P_BOLT_LMM_INF
+			# old file: 1.SNP 2.CHR 3.POS 4.Tested_Allele 5.Other_Allele 6.AF_Tested 7.BETA 8.SE 9.PVALUE
 			# new file: chromosome base_pair_location effect_allele other_allele beta standard_error effect_allele_frequency p_value rs_id
-			zcat highdim_splines/standardised_outcomes/GWAS/BOLT_results/${ADIPO}_${SS}_${CLUST}_filtered.txt.gz \
-			| awk -F'\t' -v OFS='\t' '{print $2, $3, $5, $6, $11, $12, $7, $14, $1}' \
-			> sumstats_gwas_catalog_deposit/${ADIPO}_${SS}_${CLUST}_filtered.tsv
+			awk -F'\t' -v OFS='\t' '{print $2, $3, $4, $5, $7, $8, $6, $9, $1}' highdim_splines/standardised_outcomes/GWAS/BOLT_results/${ADIPO}_${SS}_${CLUST}_final.txt \
+			> sumstats_gwas_catalog_deposit/${ADIPO}_${SS}_${CLUST}_sumstats.tsv
 			sed -i '1s/.*/chromosome\tbase_pair_location\teffect_allele\tother_allele\tbeta\tstandard_error\teffect_allele_frequency\tp_value\trs_id/' \
-			sumstats_gwas_catalog_deposit/${ADIPO}_${SS}_${CLUST}_filtered.tsv
-			gzip sumstats_gwas_catalog_deposit/${ADIPO}_${SS}_${CLUST}_filtered.tsv
+			sumstats_gwas_catalog_deposit/${ADIPO}_${SS}_${CLUST}_sumstats.tsv
+
+			# Validate
+			gwas-ssf validate --errors-out sumstats_gwas_catalog_deposit/${ADIPO}_${SS}_${CLUST}_sumstats.tsv
+			# gzip
+			gzip sumstats_gwas_catalog_deposit/${ADIPO}_${SS}_${CLUST}_sumstats.tsv
 		done
 	done
 done
